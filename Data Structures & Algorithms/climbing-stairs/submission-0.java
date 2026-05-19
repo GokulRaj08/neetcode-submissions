dfa1036f1610b8598000e7882ccbee65895e6f2a
Gokul Raj A <115525716+GokulRaj08@users.noreply.github.com>
2026-05-19 21:55:17 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2)
+        return n;
+        int one=1;
+        int two=2;
+        for(int i=3;i<=n;i++){
+            int res=one+two;
+            one=two;
+            two=res;
+
+        }
+    return two;
+    }
+}
