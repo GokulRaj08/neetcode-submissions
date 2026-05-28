bf96ca56974a6b5dd619c0653bfb6f82b621d023
Gokul Raj A <115525716+GokulRaj08@users.noreply.github.com>
2026-05-28 19:52:09 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+       HashSet<Integer> s=new HashSet<>();
+       for(int num:nums){
+
+    //     if(!s.add(num)){
+    //         return true;
+    //     }
+        
+    //    }
+    //    return false;
+            if(s.contains(num)){
+                return true;
+            }
+            s.add(num);
+       }
+       return false;
+}
+
+    }
+    
